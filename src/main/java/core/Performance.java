a92bb38d8f1bac01f3feee1b612a91310b2fdc36
MaxOhn <ohn.m@hotmail.de>
2019-03-01 19:14:49 +0100

    Calculate mania pp via osu-tools aswell

--- src/main/java/core/Performance.java
+++ src/main/java/core/Performance.java
@@ -6,6 +6,7 @@ import main.java.util.utilOsu;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
+import sun.misc.Perf;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -23,61 +24,27 @@ public class Performance {
     private BeatmapScore mapscore = null;
     private UserScore userscore = null;
     private Logger logger = Logger.getLogger(this.getClass());
-    private ManiaPerformanceCalculator maniaPerf = null;
     private int mode;
 
-    public Performance(Beatmap map, BeatmapScore score, int mode) {
+    public Performance(Beatmap map, Object score, int mode) {
         this.mode = mode;
         this.map = map;
-        this.mapscore = score;
+        if (score instanceof BeatmapScore)
+            this.mapscore = (BeatmapScore)score;
+        else if (score instanceof UserScore)
+            this.userscore = (UserScore)score;
+        else if (score instanceof UserGame)
+            this.usergame = (UserGame)score;
         prepareFiles();
-        if (mode == 0) calculateMapPP();
-        else if (mode == 2) {
-            maniaPerf = new ManiaPerformanceCalculator(createSum(score.getEnabledMods()),
-                    Main.fileInteractor.countTotalObjects(map.getBeatmapId()), (float) map.getDifficultyOverall(),
-                    score.getScore(), calculateManiaStars());
-        }
-    }
-
-    public Performance(Beatmap map, UserScore score, int mode) {
-        this.mode = mode;
-        this.map = map;
-        this.userscore = score;
-        prepareFiles();
-        if (mode == 0) calculateMapPP();
-        else if (mode == 3) {
-            maniaPerf = new ManiaPerformanceCalculator(createSum(score.getEnabledMods()),
-                    Main.fileInteractor.countTotalObjects(map.getBeatmapId()), (float)map.getDifficultyOverall(),
-                    score.getScore(), calculateManiaStars());
-            if (score.getRank().equals("F"))
-                maniaPerf.setPlayPP(0);
-        }
-    }
-
-    public Performance(Beatmap map, UserGame usergame, int mode) {
-        this.mode = mode;
-        this.map = map;
-        this.usergame = usergame;
-        prepareFiles();
-        if (mode == 0) {
-            calculateMapPP();
+        calculateMapPP();
+        if (usergame != null)
             calculatePlayPP();
-        } else if (mode == 3) {
-            maniaPerf = new ManiaPerformanceCalculator(createSum(usergame.getEnabledMods()),
-                    Main.fileInteractor.countTotalObjects(map.getBeatmapId()), (float)map.getDifficultyOverall(),
-                    usergame.getScore(), calculateManiaStars());
-            if (usergame.getRank().equals("F"))
-                maniaPerf.setPlayPP(0);
-        }
     }
 
     private int calculateCompletion() {
         int hits = utilOsu.passedObjects(usergame, mode);
         if (mapPerf == null && mode == 0) return 0;
-        if (mode == 0)
-            return (int)((double)hits*100/(double)mapPerf.getNobjects());
-        else // (mode == 3)
-            return (int)((double)hits*100/(double)maniaPerf.getNoteCount());
+        return (int)((double)hits*100/(double)mapPerf.getNobjects());
     }
 
     private void prepareFiles() {
@@ -87,15 +54,11 @@ public class Performance {
             Main.fileInteractor.downloadMap(map.getBeatmapId());
     }
 
-    // TODO
-    private double calculateManiaStars() {
-        return map.getDifficultyRating();
-    }
-
     private void calculateMapPP() {
         try {
             // E.g.: "PerformanceCalculator.dll simulate osu 171024.osu -m hd -m dt"
-            String cmdLineString = secrets.execPrefix + "dotnet " +  secrets.perfCalcPath + " simulate osu " + secrets.mapPath +
+            String cmdLineString = secrets.execPrefix + "dotnet " +  secrets.perfCalcPath + " simulate " +
+                    (mode == 0 ? "osu" : mode == 3 ? "mania" : "error") + " " + secrets.mapPath +
                     map.getBeatmapId() + ".osu";
             if (usergame != null)
                 for (Mod mod: usergame.getEnabledMods())
@@ -120,6 +83,10 @@ public class Performance {
             input.close();
             errors.close();
             pr.waitFor();
+            if (mode == 3) {
+                mapPerf.setNobjects(Main.fileInteractor.countTotalObjects(map.getBeatmapId()));
+                mapPerf.setAcc(100);
+            }
         } catch (Exception e) {
             logger.error("Something went wrong while calculating the pp of a map: " + e);
         }
@@ -134,13 +101,14 @@ public class Performance {
         if (mode == 0)
             denominator = (double)(mapPerf.getNobjects()) * 300.0D;
         else // (mode == 3)
-            denominator = maniaPerf.getNoteCount();
+            denominator = mapPerf.getNobjects();
         double res = numerator / denominator;
         return 100*Math.max(0.0D, Math.min(res, 1.0D));
     }
 
     private void calculatePlayPP() {
         boolean failedPlay = usergame.getRank().equals("F");
+        if (failedPlay && mode == 3) return; // Don't calculate failed scores of mania plays
         String mapPath = failedPlay ?
                 secrets.mapPath + "temp" + map.getBeatmapId() + usergame.getUserId() + ".osu" :
                 secrets.mapPath + map.getBeatmapId() + ".osu";
@@ -149,13 +117,17 @@ public class Performance {
                 int lastNoteTiming = Main.fileInteractor.offsetOfNote(utilOsu.passedObjects(usergame, mode), map.getBeatmapId());
                 Main.fileInteractor.copyMapUntilOffset(mapPath, map.getBeatmapId(), lastNoteTiming);
             }
-            String cmdLineString = secrets.execPrefix + "dotnet " + secrets.perfCalcPath + " simulate osu " + secrets.mapPath +
-                    map.getBeatmapId() + ".osu"
-                    + " -a " + calculateAcc()
-                    + " -c " + usergame.getMaxCombo()
-                    + " -X " + usergame.getCountMiss()
-                    + " -M " + usergame.getCount50()
-                    + " -G " + usergame.getCount100();
+            String cmdLineString = secrets.execPrefix + "dotnet " + secrets.perfCalcPath + " simulate " +
+                    (mode == 0 ? "osu" : mode == 3 ? "mania" : "error") + " " + secrets.mapPath + map.getBeatmapId() + ".osu";
+            if (mode == 0) {
+                cmdLineString += " -a " + calculateAcc()
+                        + " -c " + usergame.getMaxCombo()
+                        + " -X " + usergame.getCountMiss()
+                        + " -M " + usergame.getCount50()
+                        + " -G " + usergame.getCount100();
+            } else { // mode == 3
+                cmdLineString += " -s " + usergame.getScore();
+            }
             for (Mod mod: usergame.getEnabledMods())
                 cmdLineString += " -m " + mods_str(mod.getFlag());
             Runtime rt = Runtime.getRuntime();
@@ -172,6 +144,12 @@ public class Performance {
             input.close();
             errors.close();
             pr.waitFor();
+            if (mode == 3) {
+                playPerf.setNobjects(mapPerf.getNobjects());
+                playPerf.setCombo(usergame.getMaxCombo());
+                playPerf.setNmiss(usergame.getCountMiss());
+                playPerf.setAcc(calculateAcc());
+            }
             if (failedPlay)
                 Main.fileInteractor.deleteFile(mapPath);
         } catch(Exception e) {
@@ -180,24 +158,15 @@ public class Performance {
     }
 
     public double getTotalMapPP() {
-        if (mode == 0)
             return mapPerf == null ? 0 : mapPerf.getPp();
-        else //(mode == 3)
-            return maniaPerf.getMapPP();
     }
 
     public double getTotalPlayPP() {
-        if (mode == 0)
             return playPerf == null ? 0 : playPerf.getPp();
-        else // (mode == 3)
-            return maniaPerf.getPlayPP();
     }
 
     public int getObjectAmount() {
-        if (mode == 0)
             return mapPerf == null ? 0 : mapPerf.getNobjects();
-        else // (mode == 3)
-            return maniaPerf.noteCount;
     }
 
     public double getAcc() {
@@ -210,10 +179,10 @@ public class Performance {
 
     private static class PerfObj {
         private double acc = -1;
-        private int combo;
-        private int maxcombo;
+        private int combo = 0;
+        private int maxcombo = 0;
         private int nobjects = 0;
-        private int nmiss;
+        private int nmiss = 0;
         private double pp;
 
         PerfObj(Iterable<String> lines) {
@@ -242,135 +211,48 @@ public class Performance {
             }
         }
 
-        public double getAcc() {
+        double getAcc() {
             return acc;
+        }
+
+        void setAcc(double acc) {
+            this.acc = acc;
         }
 
         public int getCombo() {
             return combo;
         }
 
-        public int getMaxCombo() {
+        void setCombo(int combo) {
+            this.combo = combo;
+        }
+
+        int getMaxCombo() {
             return maxcombo;
         }
 
-        public int getNobjects() {
+        void setMaxCombo(int maxcombo) {
+            this.maxcombo = maxcombo;
+        }
+
+        int getNobjects() {
             return nobjects;
+        }
+
+        void setNobjects(int nobjects) {
+            this.nobjects = nobjects;
         }
 
         public int getNmiss() {
             return nmiss;
         }
 
-        public double getPp() {
+        void setNmiss(int nmiss) {
+            this.nmiss = nmiss;
+        }
+
+        double getPp() {
             return pp;
-        }
-    }
-
-    /*
-     * ---------------
-     *      Mania
-     * ---------------
-     */
-
-    // thanks yentis <3
-    private class ManiaPerformanceCalculator {
-        private int score = 0;
-        private double realScore = 0;
-        private double scoreMultiplier = 1;
-        private double stars = 0;
-        private float overallDifficulty = 0;
-        private int noteCount = 0;
-        private ArrayList<Double> ppValues = new ArrayList<>();
-        private int mods = 0;
-        private double MAX_SCORE = 1000000;
-
-        public int getNoteCount() {
-            return noteCount;
-        }
-
-        double getPlayPP() {
-            return ppValues.get(0);
-        }
-
-        public void setPlayPP(double v) {
-            ppValues.set(0, v);
-        }
-
-        double getMapPP() {
-            return ppValues.get(1);
-        }
-
-        public double getStars() {
-            return stars;
-        }
-
-        ManiaPerformanceCalculator(int mods, int noteCount, float overallDifficulty, int score, double stars) {
-            this.mods = mods;
-            this.noteCount = noteCount;
-            this.overallDifficulty = overallDifficulty;
-            this.score = score;
-            this.stars = stars;
-            if ((mods & Mod.NO_FAIL.getFlag()) > 0)
-                scoreMultiplier *= 0.5;
-            if((mods & Mod.EASY.getFlag()) > 0)
-                scoreMultiplier *= 0.5;
-            if((mods & Mod.HALF_TIME.getFlag()) > 0)
-                scoreMultiplier *= 0.5;
-            ComputePPValues();
-        }
-
-        private void ComputePPValues() {
-            double multiplier = ComputeMultiplier();
-            double strainValue = ComputeStrainValue(score);
-            double accValue = ComputeAccValue(strainValue);
-            ppValues.add(ComputeTotalPP(strainValue, accValue, multiplier));
-            strainValue = ComputeStrainValue(MAX_SCORE * scoreMultiplier);
-            accValue = ComputeAccValue(strainValue);
-            ppValues.add(ComputeTotalPP(strainValue, accValue, multiplier));
-        }
-
-        private double ComputeMultiplier() {
-            double multiplier = 0.8;
-            if ((mods & Mod.NO_FAIL.getFlag()) > 0)
-                multiplier *= 0.90;
-            if ((mods & Mod.SPUN_OUT.getFlag()) > 0)
-                multiplier *= 0.95;
-            if ((mods & Mod.EASY.getFlag()) > 0)
-                multiplier *= 0.5;
-            return multiplier;
-        }
-
-        private double ComputeStrainValue(double score) {
-            if(scoreMultiplier <= 0)
-                return 0;
-            realScore = score * (1 / scoreMultiplier);
-            double strainValue = Math.pow(5 * Math.max(1, stars / 0.2) - 4, 2.2) / 135;
-            strainValue *= 1 + 0.1 * Math.min(1, noteCount / 1500);
-            if (realScore <= 500000)
-                strainValue = 0;
-            else if (realScore <= 600000)
-                strainValue *= 0.3 * ((realScore - 500000.0) / 100000);
-            else if (realScore <= 700000)
-                strainValue *= 0.3 + 0.25 * ((realScore - 600000.0) / 100000);
-            else if (realScore <= 800000)
-                strainValue *= 0.55 + 0.2 * ((realScore - 700000.0) / 100000);
-            else if (realScore <= 900000)
-                strainValue *= 0.75 + 0.15 * ((realScore - 800000.0) / 100000);
-            else
-                strainValue *= 0.9 + 0.1 * ((realScore - 900000.0) / 100000);
-            return strainValue;
-        }
-
-        private double ComputeAccValue(double strain) {
-            double hitWindow300 = 34 + 3 * Math.min(10, Math.max(0, 10 - overallDifficulty));
-            if(hitWindow300 <= 0)
-                return 0;
-            return Math.max(0, 0.2 - ((hitWindow300 - 34) * 0.006667)) * strain * Math.pow(Math.max(0, realScore - 960000) / 40000, 1.1);
-        }
-
-        private double ComputeTotalPP(double strainValue, double accValue, double multiplier) {
-            return Math.pow(Math.pow(strainValue, 1.1) + Math.pow(accValue, 1.1), 1 / 1.1) * multiplier;
         }
     }
 }
