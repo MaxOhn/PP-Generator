5200de88129ab9f14e05f5ff9c4a192938d55eb2
MaxOhn <ohn.m@hotmail.de>
2019-10-12 19:06:36 +0200

    No prefix necessary in pms to bot

--- src/main/java/listeners/CommandListener.java
+++ src/main/java/listeners/CommandListener.java
@@ -3,13 +3,16 @@ package main.java.listeners;
 import main.java.core.commandHandler;
 import main.java.core.commandParser;
 import main.java.util.statics;
+import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class CommandListener extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event) {
-        if((event.getMessage().getContentRaw().startsWith(statics.prefix) || event.getMessage().getContentRaw().startsWith(statics.prefixAlt))
+        if((event.getChannelType() == ChannelType.PRIVATE
+                || event.getMessage().getContentRaw().startsWith(statics.prefix)
+                || event.getMessage().getContentRaw().startsWith(statics.prefixAlt))
                 && !event.getMessage().getAuthor().getId().equals(event.getJDA().getSelfUser().getId())) {
             commandHandler.handleCommand(commandParser.parser(event.getMessage().getContentRaw(), event));
         }
